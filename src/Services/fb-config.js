000f87f49dc2e3890b4914c82682f918edcc7494
sergio-cutone <52676903+sergio-cutone@users.noreply.github.com>
2021-09-21 19:31:21 -0400

    Delete fb-config.js

--- src/Services/fb-config.js
+++
@@ -1,9 +0,0 @@
-const fbConfig = {
-  apiKey: "AIzaSyAlRJDp6uIMqF2xZgRi33N7ogkZqwUWZss",
-  authDomain: "downforce-d5194.firebaseapp.com",
-  projectId: "downforce-d5194",
-  storageBucket: "downforce-d5194.appspot.com",
-  messagingSenderId: "613720572408",
-  appId: "1:613720572408:web:d63cc213ff3d51bce8960d",
-}
-module.exports = fbConfig
